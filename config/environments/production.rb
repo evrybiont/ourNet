c9b31495db78880531509369bdd4456a194c8fc9
evrybiont <evrybiont@gmail.com>
2016-01-06 20:44:55 +0200

    Fix production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,15 +79,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {host: 'ukraine-custom-culture.herokuapp.com'}
-
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.mandrillapp.com',
+    address: 'smtp.mailgun.org',
     port: 587,
-    user_name: 'evrybiont@gmail.com',
-    password: 'W10JcJEmpKaJq3VqvUmhcg',
-    domain: 'ukraine-custom-culture.herokuapp.com',
-    authentication: 'login',
+    domain: 'sandbox99da6b775fd74b3c81bee7e483d93221.mailgun.org',
+    user_name: ENV['MAILGUN_USER_NAME'],
+    password: ENV['MAILGUN_PASSWORD'],
+    authentication: 'plain',
     enable_starttls_auto: true
   }
 end
