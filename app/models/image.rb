d4b7fd544bd56858c3d33f8fb2c59962432658a7
evrybiont <evrybiont@gmail.com>
2016-02-03 00:36:27 +0200

    Add missing image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,8 @@
 class Image < ActiveRecord::Base
   belongs_to :chop
 
-  has_attached_file :data, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :data, styles: {medium: '300x300>', thumb: '100x100>'}
   validates_attachment :data, presence: true,
-                       content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
-                       size: { in: 0..20.megabytes }
+                       content_type: {content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']},
+                       size: {in: 0..20.megabytes}
 end
