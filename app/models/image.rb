d221e67e39c35006818fcb9eb4e9d69f05725339
evrybiont <evrybiont@gmail.com>
2016-06-01 17:52:37 +0300

    Upgrade style

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :chop
 
-  has_attached_file :data, styles: {medium: '300x300>', thumb: '100x100>'}
+  has_attached_file :data, styles: {medium: '250x150#', thumb: '100x100>'}
   validates_attachment :data, presence: true,
                        content_type: {content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']},
                        size: {in: 0..20.megabytes}
